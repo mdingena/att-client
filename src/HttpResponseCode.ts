bca68acdbf610c107be407b8b99b09683f558bd5
Marc Dingena <marc.dingena@gmail.com>
2022-05-26 22:48:59 +0800

    Add HTTP types

---
+++ src/HttpResponseCode.ts
@@ -0,0 +1,12 @@
+export const enum HttpResponseCode {
+  Ok = 200,
+  BadRequest = 400,
+  NotAuthorized = 401,
+  Forbidden = 403,
+  NotFound = 404,
+  MethodNotAllowed = 405,
+  NotAcceptable = 406,
+  InternalServerError = 500,
+  BadGateway = 502,
+  ServiceUnavailable = 503
+}
