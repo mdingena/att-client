d11fbc21754c4782fafcdf43107da1e7d204d83c
Marc Dingena <marc.dingena@gmail.com>
2022-06-07 16:12:38 +0800

    Add server name to ServerConnection logging

--- src/ServerConnection/ServerConnection.ts
+++ src/ServerConnection/ServerConnection.ts
@@ -34,22 +34,24 @@ export class ServerConnection extends TypedEmitter<ServerConnectionEvents> {
     const that = this;
 
     function handleError(this: WebSocket, error: Error) {
-      that.logger.error(`An error occurred on console ${that.server.id}.`, error);
+      that.logger.error(`An error occurred on console ${that.server.id} (${that.server.name}).`, error);
 
       that.emit('error', error);
     }
 
     function handlePing(this: WebSocket, data: Buffer) {
-      that.logger.debug(`Received console ${that.server.id} ping.`, data.toString());
+      that.logger.debug(`Received console ${that.server.id} (${that.server.name}) ping.`, data.toString());
       this.pong(data);
     }
 
     function handlePong(this: WebSocket, data: Buffer) {
-      that.logger.debug(`Received console ${that.server.id} pong.`, data.toString());
+      that.logger.debug(`Received console ${that.server.id} (${that.server.name}) pong.`, data.toString());
     }
 
     function handleClose(this: WebSocket, code: number, reason: Buffer) {
-      that.logger.debug(`Console ${that.server.id} is closing with code ${code}: ${reason.toString()}.`);
+      that.logger.debug(
+        `Console ${that.server.id} (${that.server.name}) is closing with code ${code}: ${reason.toString()}.`
+      );
 
       this.off('error', handleError);
       this.off('ping', handlePing);
@@ -62,7 +64,7 @@ export class ServerConnection extends TypedEmitter<ServerConnectionEvents> {
       if (isBinary) {
         // This should never happen. There is no Alta documentation about binary data being sent through WebSockets.
         that.logger.error('Puking horses! 🐴🐴🤮'); // https://thepetwiki.com/wiki/do_horses_vomit/
-        return that.logger.debug(`Received binary data on console ${that.server.id}.`, data);
+        return that.logger.debug(`Received binary data on console ${that.server.id} (${that.server.name}).`, data);
       }
 
       const message = JSON.parse(data.toString());
@@ -72,7 +74,10 @@ export class ServerConnection extends TypedEmitter<ServerConnectionEvents> {
           ? `${message.type}${typeof message.eventType === 'undefined' ? '' : `/${message.eventType}`}`
           : `command-${message.commandId}`;
 
-      that.logger.debug(`Console ${that.server.id} received ${eventName} message.`, JSON.stringify(message, null, 2));
+      that.logger.debug(
+        `Console ${that.server.id} (${that.server.name}) received ${eventName} message.`,
+        JSON.stringify(message, null, 2)
+      );
 
       if (
         that.server.status === 'connecting' &&
@@ -88,9 +93,9 @@ export class ServerConnection extends TypedEmitter<ServerConnectionEvents> {
     }
 
     function handleOpen(this: WebSocket) {
-      that.logger.debug(`Console ${that.server.id} opened.`);
+      that.logger.debug(`Console ${that.server.id} (${that.server.name}) opened.`);
 
-      that.logger.debug(`Registering console ${that.server.id} event handlers.`);
+      that.logger.debug(`Registering console ${that.server.id} (${that.server.name}) event handlers.`);
       this.on('error', handleError);
       this.on('ping', handlePing);
       this.on('pong', handlePong);
@@ -98,11 +103,11 @@ export class ServerConnection extends TypedEmitter<ServerConnectionEvents> {
 
       this.send(token, error => {
         if (typeof error !== 'undefined') {
-          that.logger.error(`Couldn't authenticate console connection.`, error);
+          that.logger.error(`Couldn't authenticate console ${that.server.id} (${that.server.name}) connection.`, error);
           return;
         }
 
-        that.logger.debug(`Authenticated console connection on server ${that.server.id}.`);
+        that.logger.debug(`Authenticated console connection on server ${that.server.id} (${that.server.name}).`);
       });
     }
 
@@ -154,7 +159,7 @@ export class ServerConnection extends TypedEmitter<ServerConnectionEvents> {
 
         const message = { id, content: command };
 
-        this.logger.debug('Sending command.', message);
+        this.logger.debug(`Sending command to ${this.server.id} (${this.server.name}).`, message);
         this.ws.send(JSON.stringify(message), error => typeof error !== 'undefined' && reject(error));
       }
     );
@@ -172,11 +177,11 @@ export class ServerConnection extends TypedEmitter<ServerConnectionEvents> {
    */
   subscribe<T extends SubscriptionEvent>(event: T, callback: (message: SubscriptionEventMessage<T>) => unknown) {
     if (this.subscribedEvents.includes(event)) {
-      this.logger.error(`Already subscribed to ${event} on server ${this.server.id}.`);
+      this.logger.error(`Already subscribed to ${event} on server ${this.server.id} (${this.server.name}).`);
       return;
     }
 
-    this.logger.info(`Subscribing to ${event} on server ${this.server.id}.`);
+    this.logger.info(`Subscribing to ${event} on server ${this.server.id} (${this.server.name}).`);
     this.subscribedEvents = [...this.subscribedEvents, event];
     this.events.on(`Subscription/${event}`, callback);
 
@@ -194,11 +199,11 @@ export class ServerConnection extends TypedEmitter<ServerConnectionEvents> {
    */
   unsubscribe<T extends SubscriptionEvent>(event: T) {
     if (!this.subscribedEvents.includes(event)) {
-      this.logger.error(`Subscription to ${event} does not exist on server ${this.server.id}.`);
+      this.logger.error(`Subscription to ${event} does not exist on server ${this.server.id} (${this.server.name}).`);
       return;
     }
 
-    this.logger.info(`Unsubscribing to ${event} on server ${this.server.id}.`);
+    this.logger.info(`Unsubscribing to ${event} on server ${this.server.id} (${this.server.name}).`);
     this.subscribedEvents = this.subscribedEvents.filter(existing => existing !== event);
     this.events.removeAllListeners(`Subscription/${event}`);
 
