f74777a0a723db9260d0270340b026909fd86dc0
Marc Dingena <marc.dingena@gmail.com>
2022-07-01 15:39:32 +0800

    Interpret console error responses

--- src/ServerConnection/CommandResultMessage.ts
+++ src/ServerConnection/CommandResultMessage.ts
@@ -9,22 +9,40 @@ type CommandParameter = {
   FullName: string;
 };
 
+type CommandResult<T> = {
+  Result?: T;
+  ResultString?: string;
+  Command?: {
+    Parameters: CommandParameter[];
+    IsProgressive: boolean;
+    ReturnType: string;
+    Priority: number;
+    Aliases: string[];
+    FullName: string;
+    Requirements: unknown[];
+    Attributes: unknown[];
+    Name: string;
+    Description: string;
+  };
+};
+
+type CommandException = {
+  Exception: {
+    ClassName: string;
+    Message: string;
+    Data: null | unknown;
+    InnerException: null | unknown;
+    HelpURL: null | unknown;
+    StackTraceString: null | unknown;
+    RemoteStackTraceString: null | unknown;
+    RemoteStackIndex: number;
+    ExceptionMethod: null | unknown;
+    HResult: number;
+    Source: null | unknown;
+  };
+};
+
 export type CommandResultMessage<T> = CommonMessage<'CommandResult'> & {
   commandId: number;
-  data: {
-    Result: T;
-    ResultString: string;
-    Command: {
-      Parameters: CommandParameter[];
-      IsProgressive: boolean;
-      ReturnType: string;
-      Priority: number;
-      Aliases: string[];
-      FullName: string;
-      Requirements: unknown[];
-      Attributes: unknown[];
-      Name: string;
-      Description: string;
-    };
-  };
+  data: CommandResult<T> | CommandException;
 };
